1bc65eb2c8e9752b65cb9907efd3839096c3d1b4
awesome-doge <awesomedogewow@gmail.com>
2022-10-08 13:33:16 +0800

    update

--- test-all.sh
+++ test-all.sh
@@ -1,8 +1,7 @@
-sudo docker buildx build --platform=linux/arm64 --progress=plain --load . -t toninstaller:debian10 -f build-debian:10.Dockerfile
-sudo docker buildx build --platform=linux/arm64 --progress=plain --load . -t toninstaller:debian11 -f build-debian:11.Dockerfile
-sudo docker buildx build --platform=linux/arm64 --progress=plain --load . -t toninstaller:ubuntu18.04 -f build-ubuntu:18.04.Dockerfile
-sudo docker buildx build --platform=linux/arm64 --progress=plain --load . -t toninstaller:ubuntu20.04 -f build-ubuntu:20.04.Dockerfile
-sudo docker buildx build --platform=linux/arm64 --progress=plain --load . -t toninstaller:ubuntu22.04 -f build-ubuntu:22.04.Dockerfile
-sudo docker buildx build --platform=linux/arm64 --progress=plain --load . -t toninstaller:centos7.9.2009 -f build-centos:centos7.9.2009.Dockerfile
-sudo docker buildx build --platform=linux/arm64 --progress=plain --load . -t toninstaller:archlinuxbase -f build-archlinux:base.Dockerfile
-
+sudo docker build -t toninstaller:debian10 -f build-debian:10.Dockerfile  . --no-cache
+sudo docker build -t toninstaller:debian11 -f build-debian:11.Dockerfile  . --no-cache
+sudo docker build -t toninstaller:ubuntu18.04 -f build-ubuntu:18.04.Dockerfile  . --no-cache
+sudo docker build -t toninstaller:ubuntu20.04 -f build-ubuntu:20.04.Dockerfile  . --no-cache
+sudo docker build -t toninstaller:ubuntu22.04 -f build-ubuntu:22.04.Dockerfile . --no-cache 
+sudo docker build -t toninstaller:centos7.9.2009 -f build-centos:centos7.9.2009.Dockerfile  . --no-cache
+sudo docker build -t toninstaller:archlinuxbase -f build-archlinux:base.Dockerfile  . --no-cache
